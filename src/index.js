034badaf99efe9c6ff1b2f5cff5a062ea4221d77
AdlerNW <47100536+AdlerNW@users.noreply.github.com>
2019-03-21 01:54:51 +0400

    traffic light on js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import "./index.scss";
+
+let lightRed = null,
+    lightYellow = null,
+    lightGreen = null;
+
+window.onload = function(){
+    lightRed = document.querySelector('.redLight');
+    lightYellow = document.querySelector('.yellowLight');
+    lightGreen = document.querySelector('.greenLight');
+
+    stop();
+}
+
+function stop(){
+    lightRed.style.opacity = '1';
+    lightGreen.style.opacity = '0.1';
+    setTimeout(ready, 10000);
+}
+
+function ready(){
+    lightRed.style.opacity = '0.1';
+    lightYellow.style.opacity = '1';
+    setTimeout(go, 3000);
+}
+
+function go(){
+    lightYellow.style.opacity = '0.1';
+    lightGreen.style.opacity = '1';
+    setTimeout(stop, 15000);
+}
+
