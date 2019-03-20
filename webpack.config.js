5e713a7c591629c7ce3b092b8a520125d11958da
AdlerNW <47100536+AdlerNW@users.noreply.github.com>
2019-03-21 00:46:07 +0400

    add necessary package

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  module: {
+    rules: [{
+      test: /\.css|\.scss$/,
+      use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+    }]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css"
+    })
+  ]
+};
